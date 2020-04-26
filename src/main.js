875ef018742b05738b09937a3e6346ce4ac7a49c
Nico Meyer <nico.meyer@i22.de>
2020-04-26 10:34:10 +0200

    adds Vue2TouchEvents

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import vueCustomElement from 'vue-custom-element';
 import 'document-register-element/build/document-register-element';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
+import Vue2TouchEvents from 'vue2-touch-events';
 import App from './App.vue';
 
 
@@ -9,7 +10,7 @@ import App from './App.vue';
 import 'swiper/css/swiper.css';
 
 Vue.use(VueAwesomeSwiper);
-
+Vue.use(Vue2TouchEvents);
 Vue.use(vueCustomElement);
 
 Vue.customElement('vue-widget', App);
