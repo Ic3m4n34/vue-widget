e7923b34253eacbd088f50281b75f5b7e4626291
Nico Meyer <nico.meyer@i22.de>
2020-04-26 10:01:04 +0200

    adds vueCustomElement

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
+import vueCustomElement from 'vue-custom-element';
 import App from './App.vue';
 
-Vue.config.productionTip = false;
+Vue.use(vueCustomElement);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+Vue.customElement('vue-widget', App);
