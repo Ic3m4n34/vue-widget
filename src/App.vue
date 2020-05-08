3fbedd22231e525fc463a656337201a34d39122b
Nico Meyer <nico.meyer@i22.de>
2020-05-08 08:46:47 +0200

    removes not needed stuff

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,16 @@
 <template>
   <div id="app">
-    <swiper
-      ref="mySwiper" :options="swiperOptions"
-      v-touch="touchHandler"
-    >
-    <swiper-slide
-      v-for="(iframe, i) in cookedIframeUrls"
-      :key="i"
-    >
-      <iframe
-        class="iframe"
-        :src="iframe"
-        name="iframe"
-        scrolling="yes"
-        frameborder="0"
-        marginheight="0px"
-        marginwidth="0px"
-      >
-      </iframe>
-    </swiper-slide>
-    <div class="swiper-pagination" slot="pagination"></div>
-  </swiper>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  props: {
-    iframeUrls: {
-      type: String,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      swiperOptions: {
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      },
-    };
-  },
-  computed: {
-    cookedIframeUrls() {
-      return this.iframeUrls.split(',').map(url => url.trim());
-    },
-    swiper() {
-      return this.$refs.mySwiper.$swiper;
-    },
-  },
-  methods: {
-    touchHandler() {
-      // alert('touched');
-    },
-  },
-  mounted() {
-    console.log('Current Swiper instance object', this.swiper);
-    // const cookedUrls = this.iframeUrls.split(',');
-    // console.log(cookedUrls);
-  },
 };
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -73,19 +20,4 @@ export default {
   min-height: 100vh;
 }
 
-.swiper-container,
-.swiper-wrapper,
-.swiper-slide,
-.iframe {
-  min-height: 100vh;
-}
-
-.swiper-slide {
-  background: lightblue;
-}
-
-.iframe {
-  width: 90%;
-  background: #ffffff;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import Vue from 'vue';
 import vueCustomElement from 'vue-custom-element';
 import 'document-register-element/build/document-register-element';
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-import Vue2TouchEvents from 'vue2-touch-events';
 import App from './App.vue';
 
-
-// import style
-import 'swiper/css/swiper.css';
-
-Vue.use(VueAwesomeSwiper);
-Vue.use(Vue2TouchEvents);
 Vue.use(vueCustomElement);
 
 Vue.customElement('vue-widget', App);
